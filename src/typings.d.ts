2c2141a0d353bb42080033f8b3331c04250f96d6
Mahesh <admin@maheshbohara.com>
2019-08-24 14:46:08 +0545

    update

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,14 +1,9 @@
-/**
- * @license
- * Copyright Akveo. All Rights Reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
 
 /* SystemJS module definition */
-declare var module: NodeModule;
-interface NodeModule {
-  id: string;
-}
+// declare var module: NodeModule;
+// interface NodeModule {
+//   id: string;
+// }
 
 // declare var tinymce: any;
 
